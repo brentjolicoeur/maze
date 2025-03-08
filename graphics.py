ada80fe68987f9fccf89c25140d669f54ba113ff
brentjolicoeur <brentjolicoeur@gmail.com>
2025-03-08 18:43:16 -0500

    L4 Complete -- Draw Lines

--- graphics.py
+++ graphics.py
@@ -21,18 +21,21 @@ class Window():
     def close(self):
         self.__running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
 class Point():
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
 class Line():
-    def __init__(self, point1, point2):
-        self.__point1 = point1
-        self.__point2 = point2
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
 
     def draw(self, canvas, fill_color):
         canvas.create_line(
-            self.__point1.x, self.__point1.y, self.__point2.x, self.__point2.y,
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y,
             fill=fill_color, width=2
         )
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    point1 = Point(5, 5)
+    point2 = Point(50,50)
+    point3 = Point(200, 100)
+    point4 = Point(100, 350)
+    line1 = Line(point1, point2)
+    line2 = Line(point3, point4)
+    line3 = Line(point1, point3)
+    line4 = Line(point2, point4)
+
+    win.draw_line(line1, "blue")
+    win.draw_line(line2, "green")
+    win.draw_line(line3, "red")
+    win.draw_line(line4, "black")
+
     win.wait_for_close()
 
 if __name__ == "__main__":
